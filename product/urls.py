b34d607510bb5c7f5835ef33c858965d043b2773
rohanaswani41 <rohanaswani41@gmail.com>
2022-11-24 17:26:09 -0500

    intermediate branch commit

--- product/urls.py
+++ product/urls.py
@@ -20,10 +20,11 @@ from django.urls import include, path
 app_name = 'myapp'
 urlpatterns = [
     path(r'', views.index , name='index'),
-    path(r'about', views.about, name='about' ),
+    path(r'about/', views.about, name='about' ),
     path(r'<str:cat_no>', views.detail_view, name='details'),
     path(r'place_order/', views.place_order, name='place_order'),
     path(r'products/',views.products_view, name='products_view'),
-    path(r'products/<str:prod_id>', views.productdetail, name="productdetail" )
+    path(r'products/<str:prod_id>', views.productdetail, name="productdetail" ),
+    path(r'login/', views.user_login, name="login" )
 ]
 
--- product/views.py
+++ product/views.py
@@ -1,32 +1,50 @@
-from http.client import HTTPResponse
-from itertools import product
-from urllib import response
 from django.shortcuts import render
-from django.shortcuts import get_object_or_404
 from django.urls import reverse
-from django.http import HttpResponseRedirect
+from django.http import HttpResponseRedirect, HttpResponse
 
 # Create your views here.
-
 from django.shortcuts import render
 
 # Import necessary classes
-from django.http import HttpResponse
-from .models import Category, Product, Client, Order
-from django.http import HttpResponseNotFound
+from .models import Category, Product
 from .forms import OrderForm, InterestForm
+
+# Import necessary classes and models
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required, user_passes_test
+
 # Create your views here.
 def index(request):
-    
     cat_list = Category.objects.all().order_by('id')[:10]
     return render(request, 'index.html', {'cat_list': cat_list, 'user_name':"batman"})
 
+# Create your views here.
+def user_login(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(username=username, password=password)
+        if user:
+            if user.is_active:
+                login(request, user)
+                return HttpResponseRedirect(reverse('myapp:index'))
+            else:
+                return HttpResponse('Your account is disabled.')
+        else:
+            return HttpResponse('Invalid login details.')
+    else:
+        return render(request, 'product/login.html')
+
+    
+@login_required
+def user_logout(request):
+    logout(request)
+    return HttpResponseRedirect(reverse(('myapp:index')))
 
 def about(request):
     return render(request, 'about.html', {'user_name':"batman"})
 
 def detail_view(request, cat_no):
-
     category = Category.objects.filter(name=cat_no).first()
     products = Product.objects.filter(category__name=cat_no).all()
     return render(request, 'detail0.html', {'cat_list': category, 'prod_list': products, 'user_name':"batman"})
