74bb52663f51d81c929855558fbbca9d5e7e83ce
rohanaswani41 <rohanaswani41@gmail.com>
2022-11-24 21:03:23 -0500

    task 3 completed

--- product/admin.py
+++ product/admin.py
@@ -1,10 +1,15 @@
 from django.contrib import admin
 from .models import Product, Client
 from django.utils.html import mark_safe
+from django.db.models import F
 
+@admin.action(description="Increase stock by 50 units")
+def increase_stock(modeladmin, request, queryset):
+    queryset.update(stock=F('stock')+50)
+    
 class ProductAdmin(admin.ModelAdmin):
     list_display = ('name', 'category', 'price', 'stock', 'available')
-
+    actions = [increase_stock]
 # Register your models here.
 admin.site.register(Product, ProductAdmin)
 
@@ -17,3 +22,4 @@ class ClientAdmin(admin.ModelAdmin):
         return mark_safe(to_return[:-2])
 
 admin.site.register(Client, ClientAdmin)
+
