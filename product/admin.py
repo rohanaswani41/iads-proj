fe61691ab84759843747cd190f986bb8f2a08dd3
rohanaswani41 <rohanaswani41@gmail.com>
2022-11-24 20:10:49 -0500

    Question 1 and 2 done

--- product/admin.py
+++ product/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from .models import Product, Client
+from django.utils.html import mark_safe
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name', 'category', 'price', 'stock', 'available')
 
 # Register your models here.
+admin.site.register(Product, ProductAdmin)
+
+class ClientAdmin(admin.ModelAdmin):
+    list_display = ('first_name', 'last_name', 'city', 'list_of_categories')
+    def list_of_categories(self,obj):
+        to_return = ''
+        for sing in obj.interested_in.all():
+            to_return += sing.name+", "
+        return mark_safe(to_return[:-2])
+
+admin.site.register(Client, ClientAdmin)
